e0ff080de254d0b5152882d53a00eb0368674f2f
sw33tLie <sw33tlie@protonmail.com>
2025-11-11 00:48:06 +0000

    fix duplicate error printing

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,6 @@ Visit https://bbscope.com for an hourly-updated list of public scopes!`,
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
