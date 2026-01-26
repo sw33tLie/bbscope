801845b0e09294ad77af978e2f0e6c4e7679f558
sw33tLie <sw33tlie@protonmail.com>
2026-01-26 02:29:03 +0000

    feat: add runtime warning for legacy version

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/sw33tLie/bbscope/cmd"
+import (
+	"fmt"
+
+	"github.com/sw33tLie/bbscope/cmd"
+)
 
 func main() {
+	fmt.Println("\033[33m[WARNING] You are using the legacy version of bbscope (v1).\033[0m")
+	fmt.Println("\033[33mDevelopment has moved to the 'main' branch (v2).\033[0m")
+	fmt.Println("\033[33mPlease update: git fetch && git checkout main\033[0m")
+	fmt.Println("")
 	cmd.Execute()
 }
